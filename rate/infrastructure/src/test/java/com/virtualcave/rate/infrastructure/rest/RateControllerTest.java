574cceee39957927461598864318c36e1d81b0b9
Carlos Devesa Pérez <carlosdevesaperez@hotmail.com>
2023-09-11 08:53:59 +0200

    [feat] TODO: Tests

--- rate/infrastructure/src/test/java/com/virtualcave/rate/infrastructure/rest/RateControllerTest.java
+++ rate/infrastructure/src/test/java/com/virtualcave/rate/infrastructure/rest/RateControllerTest.java
@@ -1,34 +1,38 @@
 /*
 package com.virtualcave.rate.infrastructure.rest;
 
+import com.virtualcave.rate.application.service.RateService;
+import com.virtualcave.rate.domain.dto.RateDto;
+import com.virtualcave.rate.domain.dto.filter.RateFilterDto;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest(cl)
-@AutoConfigureWebTestClient
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(RateController.class)
 public class RateControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
 
-    // private WireMockServer wireMockServer;
-
-    */
-/*@BeforeAll
-    public void setUp() {
-
-        //this.wireMockServer = new WireMockServer(9090);
-        //this.wireMockServer.start();
-    }*//*
-
+    @Mock
+    private RateService rateService;
 
     @Test
-    public void listRatesV1() {
+    void listRatesV1() {
 
-        this.webTestClient.get().uri("/v1/rates")
+        final RateFilterDto filterDto = new RateFilterDto();
+
+        final RateDto rateDto = new RateDto();
+
+        when(this.rateService.list(Mono.just(filterDto))).thenReturn(Flux.just(rateDto));
+
+        webTestClient.get().uri("/v1/rates")
                 .exchange()
                 .expectStatus().isEqualTo(200)
                 .expectBody()
